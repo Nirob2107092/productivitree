95b3b1652a7ce54a2df4ada724b8ddb1e1478ccc
Nirob2107092 <perfeccnirob@gmail.com>
2026-04-11 22:45:16 +0600

    initial commit

---
+++ ABCD/ABCDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABCDApp.swift
+//  ABCD
+//
+//  Created by Nahian Zarif on 11/4/26.
+//
+
+import SwiftUI
+
+@main
+struct ABCDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ABCD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ABCD
+//
+//  Created by Nahian Zarif on 11/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
